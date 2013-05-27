c571210db8d498f74f21cb9fbd5f4717b21f5d53
itiu <ValeriyBushenev@gmail.com>
2013-05-27 12:30:36 +0400

    *

--- src/main/java/org/gost19/ba2pacahon/Fetcher.java
+++ src/main/java/org/gost19/ba2pacahon/Fetcher.java
@@ -5,7 +5,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Date;
 import java.util.HashMap;
@@ -27,7 +29,8 @@ import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
-public class Fetcher {
+public class Fetcher
+{
 	private static Properties properties = new Properties();
 	private static Connection connection = null;
 	private static String dbUser;
@@ -50,24 +53,26 @@ public class Fetcher {
 	 * Выгружает данные структуры документов в виде пользовательских онтологий
 	 */
 
-	private static void fetchDocumentTypes() throws Exception {
+	private static void fetchDocumentTypes() throws Exception
+	{
 
-		ResultSet templatesRs = connection
-				.createStatement()
-				.executeQuery(
-						"select objectId FROM objects where isTemplate = 1 and actual = 1 ORDER BY timestamp ASC");
+		ResultSet templatesRs = connection.createStatement().executeQuery(
+				"select objectId FROM objects where isTemplate = 1 and actual = 1 ORDER BY timestamp ASC");
 
-		while (templatesRs.next()) {
+		while (templatesRs.next())
+		{
 			String docId = templatesRs.getString(1);
 			System.out.println("templateId = [" + docId + "]");
 
-			String docDataQuery = "select distinct content, kindOf, timestamp, recordId FROM objects where objectId = '"
-					+ docId + "' order by timestamp ASC";
+			String docDataQuery = "select distinct content, kindOf, timestamp, recordId FROM objects where objectId = '" + docId
+					+ "' order by timestamp ASC";
 			Statement st1 = connection.createStatement();
 			ResultSet templateRecordRs = st1.executeQuery(docDataQuery);
 
-			while (templateRecordRs.next()) {
-				try {
+			while (templateRecordRs.next())
+			{
+				try
+				{
 					String docXmlStr = templateRecordRs.getString(1);
 
 					// int kindOf = templateRecordRs.getInt(2);
@@ -80,14 +85,12 @@ public class Fetcher {
 					// doc.setVersionId(recordId);
 					// String dd = doc.toJSON();
 
-					BasicDBObject doc1 = new BasicDBObject("id", docId)
-							.append("version", recordId)
-							.append("type", "TEMPLATE")
-							.append("content", docXmlStr)
-							.append("timestamp", new Date(timestamp))
+					BasicDBObject doc1 = new BasicDBObject("id", docId).append("version", recordId).append("type", "TEMPLATE")
+							.append("content", docXmlStr).append("timestamp", new Date(timestamp))
 							.append("timestamp_l", timestamp);
 					ba_docs_coll.insert(doc1);
-				} catch (Exception ex) {
+				} catch (Exception ex)
+				{
 					ex.printStackTrace();
 				}
 			}
@@ -104,14 +107,16 @@ public class Fetcher {
 
 	private static int count_documents = 0;
 
-	private static void set__count_references_document() throws Exception {
+	private static void set__count_references_document() throws Exception
+	{
 		HashMap<String, Integer> count_references_document = new HashMap<String, Integer>();
 
-		DBCursor cursor = ba_docs_coll.find(new BasicDBObject("type",
-				"DOCUMENT"));
+		DBCursor cursor = ba_docs_coll.find(new BasicDBObject("type", "DOCUMENT"));
 
-		try {
-			while (cursor.hasNext()) {
+		try
+		{
+			while (cursor.hasNext())
+			{
 				DBObject obj = cursor.next();
 
 				String content = (String) obj.get("content");
@@ -120,75 +125,114 @@ public class Fetcher {
 				XmlDocument doc = (XmlDocument) XmlUtil.unmarshall(content);
 				doc.setVersionId(version);
 
-				try {
-					if (doc.getAuthorId() != null
-							&& doc.getAuthorId().length() > 0) {
-						Integer count_rd = count_references_document.get(doc
-								.getAuthorId());
+				try
+				{
+					if (doc.getAuthorId() != null && doc.getAuthorId().length() > 0)
+					{
+						Integer count_rd = count_references_document.get(doc.getAuthorId());
 						if (count_rd == null)
 							count_rd = 0;
-						count_references_document.put(doc.getAuthorId(),
-								count_rd + 1);
+						count_references_document.put(doc.getAuthorId(), count_rd + 1);
 					}
 
-					for (XmlAttribute attr : doc.getAttributes()) {
+					for (XmlAttribute attr : doc.getAttributes())
+					{
 						String type = attr.getType();
 						String link = null;
 
-						if (type.equals("LINK")) {
+						if (type.equals("LINK"))
+						{
 							link = attr.getLinkValue();
-						} else if (type.equals("ORGANIZATION")) {
+						} else if (type.equals("ORGANIZATION"))
+						{
 							link = attr.getOrganizationValue();
-						} else if (type.equals("DICTIONARY")) {
+						} else if (type.equals("DICTIONARY"))
+						{
 							link = attr.getRecordIdValue();
 						}
 
-						if (link != null && link.length() > 0) {
-							Integer count_rd = count_references_document
-									.get(link);
+						if (link != null && link.length() > 0)
+						{
+							Integer count_rd = count_references_document.get(link);
 							if (count_rd == null)
 								count_rd = 0;
 							count_references_document.put(link, count_rd + 1);
 						}
 					}
 
-				} catch (Exception ex) {
+				} catch (Exception ex)
+				{
 					ex.printStackTrace();
 				}
 			}
-		} finally {
+		} finally
+		{
 			cursor.close();
 		}
 
 		// заполним ссчетчики ссылок на документ
 		cursor = ba_docs_coll.find(new BasicDBObject("type", "DOCUMENT"));
 
-		try {
-			while (cursor.hasNext()) {
+		try
+		{
+			while (cursor.hasNext())
+			{
 				DBObject obj = cursor.next();
 
 				String id = (String) obj.get("id");
 
 				Integer count_rd = count_references_document.get(id);
 
-				if (count_rd != null && count_rd > 0) {
-					try {
-						ba_docs_coll.update(new BasicDBObject("id", id),
-								new BasicDBObject("$set", new BasicDBObject(
-										"count-references-on-document",
-										count_rd)), true, false);
-					} catch (Exception ex) {
+				if (count_rd != null && count_rd > 0)
+				{
+					try
+					{
+						ba_docs_coll.update(new BasicDBObject("id", id), new BasicDBObject("$set", new BasicDBObject(
+								"count-references-on-document", count_rd)), true, false);
+					} catch (Exception ex)
+					{
 						ex.printStackTrace();
 					}
 				}
 			}
-		} finally {
+		} finally
+		{
 			cursor.close();
 		}
 
 	}
 
-	private static int fetchDocuments() throws Exception {
+	private static String getBAObjOnTimestamp(String id, long timestamp)
+	{
+		String res = null;
+		String queryStr = "SELECT recordId FROM objects WHERE objectId = ? AND timestamp <= ? ORDER BY timestamp DESC LIMIT 1";
+		PreparedStatement ps;
+		try
+		{
+			ps = connection.prepareStatement(queryStr);
+			ps.setString(1, id);
+			ps.setLong(2, timestamp);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next())
+			{
+				res = rs.getString(1);
+			}
+			rs.close();
+			ps.close();
+		} catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if (res == null)
+			res = getBAObjOnTimestamp(id, new Date().getTime());
+
+		return res;
+	}
+
+	private static int fetchDocuments() throws Exception
+	{
 		String docsIdDataQuery;
 		ResultSet docRecordsRs;
 
@@ -197,32 +241,39 @@ public class Fetcher {
 
 		long prev_doc_count = 0;
 
-		while (timestamp_start < System.currentTimeMillis()) {
-			docsIdDataQuery = "select objectId, recordId, content, timestamp, templateVersionId, templateVersionId "
-					+ "FROM objects "
-					+ "WHERE isTemplate = 0 and actual = 1 and timestamp > "
-					+ timestamp_start
-					+ " and timestamp <= "
-					+ (timestamp_start + timestamp_delta)
-					+ " ORDER BY timestamp ASC";
-			docRecordsRs = connection.createStatement().executeQuery(
-					docsIdDataQuery);
+		while (timestamp_start < System.currentTimeMillis())
+		{
+			docsIdDataQuery = "select objectId, recordId, content, timestamp, templateId, templateVersionId " + "FROM objects "
+					+ "WHERE isTemplate = 0 and actual = 1 and timestamp > " + timestamp_start + " and timestamp <= "
+					+ (timestamp_start + timestamp_delta) + " ORDER BY timestamp ASC";
+			docRecordsRs = connection.createStatement().executeQuery(docsIdDataQuery);
 
-			while (docRecordsRs.next()) {
+			while (docRecordsRs.next())
+			{
 				String docId = docRecordsRs.getString(1);
 				String docXmlStr = docRecordsRs.getString(3);
-				String recordId = docRecordsRs.getString(2);
+				String docVersionId = docRecordsRs.getString(2);
 				Long timestamp = docRecordsRs.getLong(4);
 				String templateId = docRecordsRs.getString(5);
 				String templateVersionId = docRecordsRs.getString(6);
 
-				BasicDBObject doc1 = new BasicDBObject("id", docId)
-						.append("version", recordId)
-						.append("templateId", templateId)
-						.append("templateVersionId", templateVersionId)
-						.append("type", "DOCUMENT")
-						.append("content", docXmlStr)
-						.append("timestamp", new Date(timestamp))
+				if (templateVersionId == null)
+				{
+					// найти шаблон на дату и обновить id версии шаблона у данного документа
+					templateVersionId = getBAObjOnTimestamp(templateId, timestamp);
+
+					String queryStr = "UPDATE objects SET templateVersionId = ? WHERE objectId = ? AND recordId = ?";
+					PreparedStatement ps = connection.prepareStatement(queryStr);
+					ps.setString(1, templateVersionId);
+					ps.setString(2, docId);
+					ps.setString(3, docVersionId);
+					ps.execute();
+					ps.close();
+				}
+
+				BasicDBObject doc1 = new BasicDBObject("id", docId).append("version", docVersionId)
+						.append("templateId", templateId).append("templateVersionId", templateVersionId)
+						.append("type", "DOCUMENT").append("content", docXmlStr).append("timestamp", new Date(timestamp))
 						.append("timestamp_l", timestamp);
 				ba_docs_coll.insert(doc1);
 				count_documents++;
@@ -232,10 +283,8 @@ public class Fetcher {
 			timestamp_start += timestamp_delta;
 
 			if (count_documents - prev_doc_count > 0)
-				System.out.println("select documents, timestamp ["
-						+ timestamp_start + ":"
-						+ (timestamp_start + timestamp_delta) + "], count="
-						+ count_documents);
+				System.out.println("select documents, timestamp [" + timestamp_start + ":" + (timestamp_start + timestamp_delta)
+						+ "], count=" + count_documents);
 
 			prev_doc_count = count_documents;
 		}
@@ -243,18 +292,20 @@ public class Fetcher {
 		return count_documents;
 	}
 
-	static void init_source() throws Exception {
+	static void init_source() throws Exception
+	{
 		System.out.print("connect to source database " + dbUrl + "...");
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
-		connection = DriverManager.getConnection("jdbc:mysql://" + dbUrl,
-				dbUser, dbPassword);
+		connection = DriverManager.getConnection("jdbc:mysql://" + dbUrl, dbUser, dbPassword);
 
 		System.out.println("ok");
 	}
 
-	private static void loadProperties() {
+	private static void loadProperties()
+	{
 
-		try {
+		try
+		{
 			properties.load(new FileInputStream("ba2pacahon.properties"));
 
 			dbUser = properties.getProperty("dbUser", "ba");
@@ -262,8 +313,7 @@ public class Fetcher {
 			dbUrl = properties.getProperty("dbUrl", "localhost:3306");
 			host = properties.getProperty("rabbit-host", "localhost");
 			port = properties.getProperty("rabbit-port", "5672");
-			virtualHost = properties.getProperty("rabbit-virtualHost",
-					"bigarchive");
+			virtualHost = properties.getProperty("rabbit-virtualHost", "bigarchive");
 			userName = properties.getProperty("rabbit-userName", "ba");
 			password = properties.getProperty("rabbit-password", "123456");
 			queue = properties.getProperty("rabbit-queue", "new-search");
@@ -271,13 +321,15 @@ public class Fetcher {
 			String els[] = mongo.split(":");
 			mongodb_host = els[0];
 			mongodb_port = Integer.parseInt(els[1]);
-		} catch (IOException e) {
+		} catch (IOException e)
+		{
 			writeDefaultProperties();
 		}
 
 	}
 
-	private static void writeDefaultProperties() {
+	private static void writeDefaultProperties()
+	{
 		System.out.println("Writing default properties.");
 
 		properties.setProperty("dbUser", "ba");
@@ -285,26 +337,29 @@ public class Fetcher {
 		properties.setProperty("dbUrl", "localhost:3306");
 		properties.setProperty("mongodb", "localhost:27017");
 
-		try {
-			properties.store(new FileOutputStream("ba2pacahon.properties"),
-					null);
-		} catch (IOException e) {
+		try
+		{
+			properties.store(new FileOutputStream("ba2pacahon.properties"), null);
+		} catch (IOException e)
+		{
 			e.printStackTrace();
 		}
 	}
 
-	private static void toQueueDocumentTypes() throws Exception {
+	private static void toQueueDocumentTypes() throws Exception
+	{
 		BasicDBObject query = new BasicDBObject("type", "TEMPLATE");
 
 		DBCursor cursor = ba_docs_coll.find(query);
 
-		try {
-			while (cursor.hasNext()) {
+		try
+		{
+			while (cursor.hasNext())
+			{
 				DBObject obj = cursor.next();
 				String content = (String) obj.get("content");
 				String version = (String) obj.get("version");
-				XmlDocumentType doc = (XmlDocumentType) XmlUtil
-						.unmarshall(content);
+				XmlDocumentType doc = (XmlDocumentType) XmlUtil.unmarshall(content);
 				doc.setVersionId(version);
 
 				String dd = doc.toJSON();
@@ -312,63 +367,82 @@ public class Fetcher {
 
 				// System.out.println(dd);
 			}
-		} finally {
+		} finally
+		{
 			cursor.close();
 		}
 	}
 
-	private static void toQueueDocuments(BasicDBObject query,
-			String is_processed_links) throws Exception {
+	private static void toQueueDocuments(BasicDBObject query, String is_processed_links) throws Exception
+	{
 		DBCursor cursor = ba_docs_coll.find(query);
 
-		try {
-			while (cursor.hasNext()) {
+		int count = 0;
+		
+		try
+		{
+			while (cursor.hasNext())
+			{
 				DBObject obj = cursor.next();
 
 				String content = (String) obj.get("content");
 				String id = (String) obj.get("id");
 				String version = (String) obj.get("version");
+				String typeVersion = (String) obj.get("templateVersionId");
 
-				Object count_references_on_document_o = obj
-						.get("count-references-on-document");
+				Object count_references_on_document_o = obj.get("count-references-on-document");
 
 				int count_references_on_document = 0;
 
-				if (count_references_on_document_o != null) {
-					if (count_references_on_document_o instanceof java.lang.Integer) {
+				if (count_references_on_document_o != null)
+				{
+					if (count_references_on_document_o instanceof java.lang.Integer)
+					{
 						count_references_on_document = (Integer) count_references_on_document_o;
 					}
-					if (count_references_on_document_o instanceof java.lang.Double) {
-						count_references_on_document = ((Double) count_references_on_document_o)
-								.intValue();
+					if (count_references_on_document_o instanceof java.lang.Double)
+					{
+						count_references_on_document = ((Double) count_references_on_document_o).intValue();
 					}
 
 				}
 
-				XmlDocument doc = (XmlDocument) XmlUtil.unmarshall(content);
-				doc.setVersionId(version);
+				if ((is_processed_links.equals("N") && count_references_on_document > 0) || is_processed_links.equals("Y"))
+				{
+//					if (is_processed_links.equals("Y") && count == 100)
+//						break;
+					
+					XmlDocument doc = (XmlDocument) XmlUtil.unmarshall(content);
+					doc.setVersionId(version);
+					doc.setTypeVersionId(typeVersion);
 
-				try {
-					JSONObject jo = doc.getJSON();
-					jo.put("is-processed-links", is_processed_links);
+					try
+					{
+						JSONObject jo = doc.getJSON();
+						jo.put("is-processed-links", is_processed_links);
 
-					if (count_references_on_document > 0)
-						jo.put("is-cached", "Y");
+						if ((is_processed_links.equals("N") && count_references_on_document > 0))
+							jo.put("is-cached", "Y");
 
-					String dd = JSONObject.toJSONString(jo);
-					messagingManager.sendMessage(queue, dd);
-				} catch (Exception ex) {
-					ex.printStackTrace();
+						String dd = JSONObject.toJSONString(jo);
+						messagingManager.sendMessage(queue, dd);
+						
+						count ++;
+					} catch (Exception ex)
+					{
+						ex.printStackTrace();
+					}
 				}
-
 				// System.out.println(dd);
 			}
-		} finally {
+		} finally
+		{
 			cursor.close();
 		}
 	}
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception
+	{
 		messagingManager = new AMQPMessagingManager();
 		loadProperties();
 
@@ -377,8 +451,10 @@ public class Fetcher {
 
 		ba_docs_coll = pacahon_db.getCollection("ba_docs");
 
-		if (ba_docs_coll.count() == 0) {
-			try {
+		if (ba_docs_coll.count() == 0)
+		{
+			try
+			{
 				System.out.println("start sql ba xml -> mongo ba json");
 				System.out.println("init_source");
 				init_source();
@@ -390,15 +466,16 @@ public class Fetcher {
 				set__count_references_document();
 				System.out.println("end start sql ba xml -> mongo ba json");
 				// Thread.currentThread().sleep(999999999);
-			} catch (Exception ex) {
+			} catch (Exception ex)
+			{
 				System.out.println("Error !");
 				ex.printStackTrace(System.out);
 			}
 
-		} else {
+		} else
+		{
 			System.out.println("start ba json -> pacahon");
-			messagingManager.init(host, Integer.parseInt(port), virtualHost,
-					userName, password, responseWaitingLimit, null);
+			messagingManager.init(host, Integer.parseInt(port), virtualHost, userName, password, responseWaitingLimit, null);
 			System.out.println("TEMPLATE pass I");
 			toQueueDocumentTypes();
 			System.out.println("TEMPLATE pass II");
